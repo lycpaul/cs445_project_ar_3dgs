ca9b7c047b396211134eee082221334454226176
lycpaul <ycleepaul@gmail.com>
2025-05-10 18:58:07 -0700

    added blender mesh rotation update

--- blender_example/locate_mesh.py
+++ blender_example/locate_mesh.py
@@ -1,7 +1,7 @@
 import bpy
 import math
 import mathutils
-from mathutils import Vector
+import numpy as np
 
 
 def set_object_trans(ref_frame, trans):
@@ -13,15 +13,11 @@ def set_object_trans(ref_frame, trans):
     obj.location = world_position
 
 
-def set_object_mat(ref_frame, trans, rot):
-    # euler to rotation matrix
-    eul = mathutils.Euler(rot, 'XYZ')
-    # mat = eul.to_matrix().to_4x4()
-    # mat.translation = trans
-    scale = Vector((0.005, 0.005, 0.005))
-    
-    mat = mathutils.Matrix.LocRotScale(trans, eul, scale)
-
+def set_object_mat(ref_frame,
+                   trans,
+                   rot=mathutils.Matrix(),
+                   scale=mathutils.Vector((0.005, 0.005, 0.005))):
+    mat = mathutils.Matrix.LocRotScale(trans, rot, scale)
     obj = bpy.data.objects['r2d2']
     obj.matrix_world = ref_frame @ mat
 
@@ -35,16 +31,30 @@ def set_camera_view(trans, rot):
     bpy.context.scene.camera.matrix_world = mat
 
 
+def get_cv_rot_mat() -> mathutils.Matrix:
+    R_cv = np.array([[0.88278894,  0.2500285, -0.39770521],
+                    [0.05772972, -0.89791887, -0.43635877],
+                    [-0.46620914,  0.36225329, -0.80710693]])
+    R_cv2blender = np.array([[1, 0, 0],
+                            [0, -1, 0],
+                            [0, 0, -1]])
+    R_rotx90 = np.array([[1, 0, 0],
+                        [0, 0, -1],
+                        [0, 1, 0]])
+    return mathutils.Matrix(R_cv2blender @ R_cv @ R_rotx90)
+
+
 # default view point
 mat_cam = camera = bpy.context.scene.camera.matrix_world
-set_camera_view(Vector((-1, 3, 8)), (40, 15, 175))
+set_camera_view(mathutils.Vector((-1, 3, 8)), (40, 15, 175))
 
 # OpenCV result
 # set_object_view(mat_cam, Vector((0.312, -0.08372, -5.17)))
 
-# Manual result
-# set_object_trans(mat_cam, Vector((1.2989, -0.2734, -4.2197)))
-set_object_mat(mat_cam, Vector((1.2989, -0.2734, -4.2197)), (1.0024, -0.4284, -0.1314))
+# euler to rotation matrix
+R_mat = mathutils.Euler((1.0024, -0.4284, -0.1314), 'XYZ').to_matrix()
+set_object_mat(mat_cam, mathutils.Vector((1.2989, -0.2734, -4.2197)),
+               R_mat)
 
 # debug
 print("mat_cam")
@@ -57,4 +67,4 @@ print(mat_obj)
 print("obj_T_cam")
 obj_T_cam = mat_cam.inverted() @ mat_obj
 print(obj_T_cam)
-print(obj_T_cam.to_euler())
+print(obj_T_cam.to_euler().to_matrix())
